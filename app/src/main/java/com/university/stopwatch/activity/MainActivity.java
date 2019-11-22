00f480c7641a449a17995191102b3655f52c06b8
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-11-22 19:33:17 +0300

    refactoring

--- app/src/main/java/com/university/stopwatch/activity/MainActivity.java
+++ app/src/main/java/com/university/stopwatch/activity/MainActivity.java
@@ -1,17 +1,14 @@
-package com.university.stopwatch;
+package com.university.stopwatch.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.PersistableBundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.university.stopwatch.R;
 
 public class MainActivity extends AppCompatActivity {
 
